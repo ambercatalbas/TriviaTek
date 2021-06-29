b87a0370e55e84803a68652fc1aabb810cd25a7e
ambercatalbas <ambercatalbas@gmail.com>
2021-06-29 16:49:39 +0300

    Initial Commit

---
+++ TriviaTek/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TriviaTek
+//
+//  Created by Yasemin YEL on 29.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
